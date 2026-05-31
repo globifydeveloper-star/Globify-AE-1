f3c1fe2b371b60ddb7fec6fdfbf4df5cee58d47e
SK707 <krish@globify.in>
2026-05-31 13:32:37 +0400

    Update Page

--- src/app/app-development/page.tsx
+++ src/app/app-development/page.tsx
@@ -1,11 +1,5 @@
-import ServiceSchema from "@/components/ServiceSchema";
 import AppDevelopmentClient from "./AppDevelopmentClient";
 
 export default function Page() {
-  return (
-    <>
-      <ServiceSchema name="App Development Services" description="Expert App Development services by Globify UAE." url="https://www.globify.ae/app-development" />
-      <AppDevelopmentClient />
-    </>
-  );
+  return <AppDevelopmentClient />;
 }
